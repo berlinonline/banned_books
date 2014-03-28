2c108a9af01489ed34b6ea2313663fae6db98da5
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2014-03-28 13:29:06 +0100

    Set index page title

--- applications/frontend/project/modules/Common/impl/Index/IndexSuccessView.class.php
+++ applications/frontend/project/modules/Common/impl/Index/IndexSuccessView.class.php
@@ -9,6 +9,8 @@ class Common_Index_IndexSuccessView extends CommonBaseView
 {
     public function executeHtml(AgaviRequestDataHolder $parameters)
     {
+        $this->setAttribute('_title', 'Startseite');
+
         $this->setupHtml($parameters);
     }
 }
